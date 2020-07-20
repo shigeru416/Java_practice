dd227bf56637e45c59f04c3950715545d09f6f90
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-07-20 19:19:33 +0900

    practice

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		String word = "Max";
+		System.out.println(word);
+		word = "world";
+		System.out.println(word);
+		int num = 6;
+		System.out.println(num);
+	}
+}
