959271e339369418b2cb455ad73bdc0d35657259
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-08-20 22:13:11 +0900

    practice

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("RPG: すっきり魔王征伐");
+		System.out.println("Ver.0.1 by渚");
+		System.out.println("＜ただいま鋭意学習・製作中＞");
+		System.out.println("プログラムを終了します。");
+	}
+}
