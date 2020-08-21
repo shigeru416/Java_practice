738b74b3184e9b16c734301a985bab92bd2c7466
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-08-21 23:11:24 +0900

    practice

---
+++ Main2.java
@@ -0,0 +1,9 @@
+
+public class Main2 {
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		int pie = 5;
+		System.out.println("半径"+pie+"cmの面積は、");
+		System.out.println(pie*pie*PI);
+	}
+}
