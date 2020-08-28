b32f800b6a1d548d497a926d2eacf8b3534dfb88
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-08-28 22:41:33 +0900

    practice

---
+++ Confirmation.java
@@ -0,0 +1,11 @@
+
+public class Confirmation {
+	public static void main(String[] args) {
+		boolean doorclose = true;
+
+		while (doorclose == true) {
+			System.out.println("ノックする");
+			System.out.println("１分まつ");
+		}
+	}
+}
