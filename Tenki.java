6ef50b28d63da0875b2c03fd88fdfb148d6a08c0
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-08-29 23:12:30 +0900

    practice

---
+++ Tenki.java
@@ -0,0 +1,11 @@
+
+public class Tenki {
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯しますか？");
+		}else {
+			System.out.println("DVDを見ます。");
+		}
+	}
+}
